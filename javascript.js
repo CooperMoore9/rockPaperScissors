5de150e3ca9dc7248d21c90fdf6c2976be1daec2
CooperMoore9 <thefryman9@gmail.com>
2022-06-23 00:14:35 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,12 +9,12 @@
             // - if user didn't type any of the expected answers alert 'not a answer' and let them type again
         // - get random result from bot
             // - get random number
-            // - assign number to a choice in rbs
+                // - console.log(Math.floor(Math.random()* 3));
+                    // gets random number between 0-2
+            // - assign number to a choice in rps
             // - return choice for round
         // - compare results
+            // - get result from user and bot and if user chose the winning choice over the bot, report they won, else they lost
         // - console.log() results tell user if they won or lost
         // - 5 rounds total; at the end report who won more rounds
         
-
-// console.log(Math.floor(Math.random()* 3));
-    // gets random number between 0-2
