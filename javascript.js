24ac742bf50ab496ba75f86f3c558c77ea8df982
CooperMoore9 <thefryman9@gmail.com>
2022-06-22 23:34:29 -0500

    setup
    no actual programming yet

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// Rock Paper Scissors
+
+    // TODO
+        // - Get input from user
+            // - popup window for user to provide input
+            // - make input case-insensitive
+            // - store input in variable
+            // - use input to play against bot
+            // - if user didn't type any of the expected answers alert 'not a answer' and let them type again
+        // - get random result from bot
+            // - get random number
+            // - assign number to a choice in rbs
+            // - return choice for round
+        // - compare results
+        // - console.log() results tell user if they won or lost
+        // - 5 rounds total; at the end report who won more rounds
+        
+
+// console.log(Math.floor(Math.random()* 3));
+    // gets random number between 0-2
