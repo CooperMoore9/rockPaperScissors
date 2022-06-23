66782300733c1a0bd196c43b30e31f1b5cd0ec4a
CooperMoore9 <thefryman9@gmail.com>
2022-06-23 14:00:10 -0500

    Added botChoice
    makes bot chose at random. done similarly to player choice

--- javascript.js
+++ javascript.js
@@ -10,26 +10,42 @@
     
             // - if user didn't type any of the expected answers alert 'not a answer' and let them type again
 
-    function playerSelectionFunction(){
+    function playerSelectionF(){
     let playerSelection = prompt('Enter: Rock, Paper, or Scissors').toLowerCase().trim();
     if(playerSelection === 'rock' || playerSelection === 'paper'|| playerSelection === 'scissors'){
         return playerSelection;
     } else {
         alert('wrong')
-        return playerSelectionFunction();
+        return playerSelectionF();
     }
 }
 
-    let playerChoice = playerSelectionFunction();
-    console.log(playerChoice);
+    let playerChoice = playerSelectionF();
+    // console.log(playerChoice);
 
-        // - use input to play against bot
+
         // - get random result from bot
+    function botChoiceF(){
+        let botNumber = Math.floor(Math.random() * 3)
+        if(botNumber === 0){
+            return 'rock';
+        }else if(botNumber === 1){
+            return 'paper';
+        }else if(botNumber === 2){
+            return 'scissors'
+        }
+    }
+
+    let botChoice = botChoiceF();
+    console.log(botChoice);
+
         //     - get random number
         //         - console.log(Math.floor(Math.random()* 3));
         //             gets random number between 0-2
         //     - assign number to a choice in rps
         //     - return choice for round
+        // - use input from bot and user to play
         // - compare results
         // - console.log() results tell user if they won or lost
         // - 5 rounds total; at the end report who won more rounds
+
