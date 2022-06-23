49478a84f60fb8389f1d1ef185c7caf4907ce4af
CooperMoore9 <thefryman9@gmail.com>
2022-06-23 14:25:14 -0500

    Rounds work
    can play rock paper scissors with bot

--- javascript.js
+++ javascript.js
@@ -7,25 +7,26 @@
             // - popup window for user to provide input
             // - store input in variable
             // - make input case-insensitive
-    
             // - if user didn't type any of the expected answers alert 'not a answer' and let them type again
-
     function playerSelectionF(){
-    let playerSelection = prompt('Enter: Rock, Paper, or Scissors').toLowerCase().trim();
-    if(playerSelection === 'rock' || playerSelection === 'paper'|| playerSelection === 'scissors'){
-        return playerSelection;
-    } else {
-        alert('wrong')
-        return playerSelectionF();
+        let playerSelection = prompt('Enter: Rock, Paper, or Scissors').toLowerCase().trim();
+        if(playerSelection === 'rock' || playerSelection === 'paper'|| playerSelection === 'scissors'){
+            return playerSelection;
+        } else {
+            alert('wrong')
+            return playerSelectionF();
+        }
     }
-}
 
     let playerChoice = playerSelectionF();
-    // console.log(playerChoice);
-
 
         // - get random result from bot
-    function botChoiceF(){
+                // - get random number
+                // - console.log(Math.floor(Math.random()* 3));
+                    // - gets random number between 0-2
+        // - assign number to a choice in rps
+        // - return choice for round
+    function botSelectionF(){
         let botNumber = Math.floor(Math.random() * 3)
         if(botNumber === 0){
             return 'rock';
@@ -36,16 +37,41 @@
         }
     }
 
-    let botChoice = botChoiceF();
-    console.log(botChoice);
 
-        //     - get random number
-        //         - console.log(Math.floor(Math.random()* 3));
-        //             gets random number between 0-2
-        //     - assign number to a choice in rps
-        //     - return choice for round
+    let botChoice = botSelectionF();
+
         // - use input from bot and user to play
         // - compare results
         // - console.log() results tell user if they won or lost
+    function playRound(playerChoice, botChoice){
+        if(playerChoice === botChoice){
+            console.log('tie')
+
+        }else if(playerChoice === 'rock' && botChoice !== 'paper'){
+            console.log('player win');
+            
+        }else if(playerChoice === 'paper' && botChoice !== 'scissors'){
+            console.log('player win');
+
+        }else if(playerChoice === 'scissors' && botChoice !== 'rock'){
+            console.log('player win')
+
+        }else{
+            console.log('bot win')
+        }
+
+    }   
+
+    console.log('player chose ' + playerChoice);
+    console.log('bot chose ' + botChoice);
+    // playRound(playerChoice, botChoice);
+
         // - 5 rounds total; at the end report who won more rounds
 
+function game(){
+    for (let i = 0; i < 5; i++){
+        
+    }
+
+
+}
