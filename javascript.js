83b9be3e4f63da25cd4647ec2db94ea03cda1768
CooperMoore9 <thefryman9@gmail.com>
2022-06-23 13:06:01 -0500

    check if user inputs valid answer

--- javascript.js
+++ javascript.js
@@ -1,20 +1,29 @@
 // Rock Paper Scissors
 
     // TODO
+
+
         // - Get input from user
             // - popup window for user to provide input
-            // - make input case-insensitive
             // - store input in variable
-            // - use input to play against bot
+            // - make input case-insensitive
+    let playerSelection = prompt('Enter: Rock, Paper, or Scissors').toLowerCase().trim();
             // - if user didn't type any of the expected answers alert 'not a answer' and let them type again
+    if(playerSelection === 'rock' || playerSelection === 'paper'|| playerSelection === 'scissors'){
+        console.log('right!');
+    } else {
+        console.log('wrong!');
+    }
+
+        // - use input to play against bot
         // - get random result from bot
-            // - get random number
-                // - console.log(Math.floor(Math.random()* 3));
-                    // gets random number between 0-2
-            // - assign number to a choice in rps
-            // - return choice for round
+        //     - get random number
+        //         - console.log(Math.floor(Math.random()* 3));
+        //             gets random number between 0-2
+        //     - assign number to a choice in rps
+        //     - return choice for round
         // - compare results
-            // - get result from user and bot and if user chose the winning choice over the bot, report they won, else they lost
         // - console.log() results tell user if they won or lost
         // - 5 rounds total; at the end report who won more rounds
-        
+
+    console.log(playerSelection)
