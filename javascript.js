0e6a7e7a9030c069a85b5875c3cf15457b56aaea
CooperMoore9 <thefryman9@gmail.com>
2023-03-06 13:07:58 -0600

    prettier

--- javascript.js
+++ javascript.js
@@ -1,97 +1,94 @@
-// Rock Paper Scissor
-    // TODO
-    // make site look better
-            
 let playerScore = 0;
 let botScore = 0;
-let playerChoice = '';
-const body = document.querySelector('body')
+let playerChoice = "";
+const body = document.querySelector("body");
 
-const clickedButton = document.querySelectorAll('button');
-    clickedButton.forEach((button) => {
-        button.addEventListener('click', () => {
-        playerChoice = button.id;
-        playGame();
-    })
-})
+const clickedButton = document.querySelectorAll("button");
+clickedButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    playerChoice = button.id;
+    playGame();
+  });
+});
 
-
-
-
-function botSelectionF(){
-    let botNumber = Math.floor(Math.random() * 3)
-    if(botNumber === 0){
-        return 'rock';
-    }else if(botNumber === 1){
-        return 'paper';
-    }else if(botNumber === 2){
-        return 'scissors';
-    }
-}
-        
-function playRound(playerChoice, botChoice){
-    if(playerChoice === botChoice){
-        return 'tie';
-    }else if(playerChoice === 'rock' && botChoice !== 'paper'){
-        return 'player';
-    }else if(playerChoice === 'paper' && botChoice !== 'scissors'){
-        return 'player';
-    }else if(playerChoice === 'scissors' && botChoice !== 'rock'){
-        return 'player';
-    }else{
-        return 'bot';
-    }
-
-}   
-
-function disableButtons(){
-    document.getElementById('rock').disabled = true;
-    document.getElementById('paper').disabled = true;
-    document.getElementById('scissors').disabled = true;
-    restartButton();
+function botSelectionF() {
+  let botNumber = Math.floor(Math.random() * 3);
+  if (botNumber === 0) {
+    return "rock";
+  } else if (botNumber === 1) {
+    return "paper";
+  } else if (botNumber === 2) {
+    return "scissors";
+  }
 }
 
-function restartButton(){
-    if(document.getElementById('rock').disabled === true){
-        const reload = document.createElement('button');
-        reload.textContent = 'RESTART';
-        reload.classList.add('restartButton')
-        body.appendChild(reload);
-
-        reload.addEventListener('click', () =>  {
-            location.reload();
-        })
-
-    }
+function playRound(playerChoice, botChoice) {
+  if (playerChoice === botChoice) {
+    return "tie";
+  } else if (playerChoice === "rock" && botChoice !== "paper") {
+    return "player";
+  } else if (playerChoice === "paper" && botChoice !== "scissors") {
+    return "player";
+  } else if (playerChoice === "scissors" && botChoice !== "rock") {
+    return "player";
+  } else {
+    return "bot";
+  }
 }
 
-function playGame(){
-    let botChoice = botSelectionF();
-    let winner = playRound(playerChoice, botChoice);
+function disableButtons() {
+  document.getElementById("rock").disabled = true;
+  document.getElementById("paper").disabled = true;
+  document.getElementById("scissors").disabled = true;
+  restartButton();
+}
 
-    if(winner === 'player'){
-        playerScore++;
-        document.getElementById('whoWonTheRound').textContent = `Player chose ${playerChoice}, bot chose ${botChoice}, Player Wins!`
-    }else if (winner === 'bot'){
-        botScore++;
-        document.getElementById('whoWonTheRound').textContent = `Player chose ${playerChoice}, Bot chose ${botChoice}, Bot Wins!`
-    }else{
-        document.getElementById('whoWonTheRound').textContent = `Player chose ${playerChoice}, bot chose ${botChoice}, tie`
-    }
-    
-    document.getElementById('playerScore').textContent = `Player Score = ${playerScore}` ;
-    document.getElementById('botScore').textContent = `Bot Score = ${botScore}` ;
+function restartButton() {
+  if (document.getElementById("rock").disabled === true) {
+    const reload = document.createElement("button");
+    reload.textContent = "RESTART";
+    reload.classList.add("restartButton");
+    body.appendChild(reload);
 
-    if(playerScore === 5 && botScore === 5){
-        document.getElementById('middleText').textContent = `Tied`;
-        disableButtons();
-    }
-    else if(playerScore === 5 && botScore < 5){
-        document.getElementById('middleText').textContent = `player winnah`;
-        disableButtons();
-    }else if(playerScore < 5 && botScore === 5){
-        document.getElementById('middleText').textContent = `bot winnah`;
-        disableButtons();
-    }
+    reload.addEventListener("click", () => {
+      location.reload();
+    });
+  }
+}
 
+function playGame() {
+  let botChoice = botSelectionF();
+  let winner = playRound(playerChoice, botChoice);
+
+  if (winner === "player") {
+    playerScore++;
+    document.getElementById(
+      "whoWonTheRound"
+    ).textContent = `Player chose ${playerChoice}, bot chose ${botChoice}, Player Wins!`;
+  } else if (winner === "bot") {
+    botScore++;
+    document.getElementById(
+      "whoWonTheRound"
+    ).textContent = `Player chose ${playerChoice}, Bot chose ${botChoice}, Bot Wins!`;
+  } else {
+    document.getElementById(
+      "whoWonTheRound"
+    ).textContent = `Player chose ${playerChoice}, bot chose ${botChoice}, tie`;
+  }
+
+  document.getElementById(
+    "playerScore"
+  ).textContent = `Player Score = ${playerScore}`;
+  document.getElementById("botScore").textContent = `Bot Score = ${botScore}`;
+
+  if (playerScore === 5 && botScore === 5) {
+    document.getElementById("middleText").textContent = `Tied`;
+    disableButtons();
+  } else if (playerScore === 5 && botScore < 5) {
+    document.getElementById("middleText").textContent = `player winnah`;
+    disableButtons();
+  } else if (playerScore < 5 && botScore === 5) {
+    document.getElementById("middleText").textContent = `bot winnah`;
+    disableButtons();
+  }
 }
